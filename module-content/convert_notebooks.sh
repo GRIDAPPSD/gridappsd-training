4a714566da923c943dd2e9df4af175c4d85f4e4c
Ubuntu <ubuntu@ip-10-15-124-149.us-west-2.compute.internal>
2021-07-07 03:47:05 +0000

    fixed image dir

--- module-content/convert_notebooks.sh
+++ module-content/convert_notebooks.sh
@@ -8,7 +8,7 @@ jupyter nbconvert installation/windows10/1.4W--Lesson-1.4--Installing-Python-Tut
 
 jupyter nbconvert installation/1.5--Lesson-1.5--Using-GridAPPS-D-Viz.ipynb --output-dir='./docs/source/installation' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert installation/1.6--Lesson-1.6--Docker-Shortcuts.ipynb --output-dir='./docs/source/installation' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
-cp -vr ./installation/images ./docs/source/installation/images
+cp -vr ./installation/images ./docs/source/installation
 
 jupyter nbconvert overview/2.1--Lesson-2.1--Intro-to-GridAPPS-D.ipynb --output-dir='./docs/source/overview' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert overview/2.2--Lesson-2.2--GridAPPS-D-Architecture.ipynb --output-dir='./docs/source/overview' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
@@ -16,16 +16,16 @@ jupyter nbconvert overview/2.3--Lesson-2.3--GridAPPS-D-Python-Library.ipynb --ou
 jupyter nbconvert overview/2.4--Lesson-2.4--GridAPPS-D-Application-Structure.ipynb --output-dir='./docs/source/overview' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert overview/2.5--Lesson-2.5--GridAPPS-D-Service-Structure.ipynb --output-dir='./docs/source/overview' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert overview/2.6--Lesson-2.6--Common-Information-Model.ipynb --output-dir='./docs/source/overview' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
-cp -vr ./overview/images ./docs/source/overview/images
+cp -vr ./overview/images ./docs/source/overview
 
 jupyter nbconvert api_usage/3.1--Lesson-3.1--API-Communication-Channels.ipynb --output-dir='./docs/source/api_usage' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert api_usage/3.2--Lesson-3.2--API-Message-Structure --output-dir='./docs/source/api_usage' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert api_usage/3.3--Lesson-3.3--Using-the-PowerGrid-Models-API --output-dir='./docs/source/api_usage' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert api_usage/3.4--Lesson-3.4--Using-the-Configuration-File-API --output-dir='./docs/source/api_usage' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 jupyter nbconvert api_usage/3.5--Lesson-3.5--Creating-Running-Simulation-API.ipynb --output-dir='./docs/source/api_usage' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
-cp -vr ./api_usage/images ./docs/source/overview/images
+cp -vr ./api_usage/images ./docs/source/overview
 
-cp -vr ./images ./docs/source/images
+cp -vr ./images ./docs/source
 
 #jupyter nbconvert  --output-dir='./docs/source/' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
 #jupyter nbconvert  --output-dir='./docs/source/' --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags 'remove_cell' --to notebook
